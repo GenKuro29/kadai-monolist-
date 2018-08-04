4ca7e5420272f30952af39a532c5592e7916d5ac
Gen.K <kamakuraha1192@hotmail.com>
2018-08-04 02:53:06 +0000

    have

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -16,6 +16,7 @@
                             <div class="buttons text-cneter">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
+                                    @include('items.have_button', ['item' => $item])
                                 @endif
                             </div>
                         </div>
